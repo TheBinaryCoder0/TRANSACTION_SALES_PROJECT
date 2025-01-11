4fb3fcac29294dfce47f3ce3cc14ec59c48f5383
Aman Shahi <118298784+TheBinaryCoder0@users.noreply.github.com>
2025-01-11 17:08:39 +0530

    Update transactionRoutes.js

--- backend/src/routes/transactionRoutes.js
+++ backend/src/routes/transactionRoutes.js
@@ -1,1 +1,32 @@
+const express = require('express');
+const {
+  initializeDb,
+  getTransactions,
+  getStatistics,
+  getPriceRangeData,
+  getCategoryData,
+  getCombinedData
+} = require('../controllers/transactionController');
 
+const router = express.Router();
+
+// ROUTES:-
+
+router.post('/initialize-db', initializeDb);
+
+
+router.get('/transactions', getTransactions);
+
+
+router.get('/transactions/statistics', getStatistics);
+
+
+router.get('/transactions/bar-chart', getPriceRangeData);
+
+
+router.get('/transactions/pie-chart', getCategoryData);
+
+
+router.get('/transactions/combined', getCombinedData);
+
+module.exports = router;
