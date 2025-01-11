fb9f0f917bece1b7f6cd3c05105bd1683ff0e362
Aman Shahi <118298784+TheBinaryCoder0@users.noreply.github.com>
2025-01-11 17:07:25 +0530

    Update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,1 +1,17 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Atlas Connected");
+  } catch (error) {
+    console.error("Error connecting to MongoDB Atlas:", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
